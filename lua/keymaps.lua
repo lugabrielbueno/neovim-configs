0398eed20c45a7cb0113985132432aa8a59ec133
Gabriel Bueno <lbueno.gabriel@gmail.com>
2022-07-24 14:20:39 -0300

    keys

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -3,7 +3,8 @@ vim.api.nvim_set_keymap("n", "<c-f>", ":NvimTreeFocus<cr>", { noremap = true, si
 
 --Find files using Telescope command-line sugar.
 vim.api.nvim_set_keymap("n", "<leader>ff", ":Telescope find_files<cr>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>fg", ":Telescope live_grep<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>lg", ":Telescope live_grep<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>gs", ":Telescope grep_string<cr>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>fb", ":Telescope buffers<cr>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>fh", ":Telescope help_tags<cr>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<space>fm", ":lua vim.lsp.buf.formatting()<cr>", { noremap = true, silent = true })
