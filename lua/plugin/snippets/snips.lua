128b7d9f999e2367acd8f7b55f40eec70c28b1bb
Lucas Gabriel Bueno <lbueno.gabriel@gmail.com>
2022-08-19 12:20:56 -0300

    html5 snip

--- lua/plugin/snippets/snips.lua
+++ lua/plugin/snippets/snips.lua
@@ -44,6 +44,22 @@ ls.add_snippets("php", {
 		i(0),
 		t({ "", "}", "" }),
 	}),
+	s("html", {
+		t({ "<!DOCTYPE html>", "" }),
+		t({ "<html lang='en'>", "" }),
+		t({ "<head>", "" }),
+		t({ "  <meta charset='UTF-8'>", "" }),
+		t({ "  <meta http-equiv='X-UA-Compatible' content='IE=edge'>", "" }),
+		t({ "  <meta name='viewport' content='width=device-width, initial-scale=1.0'>", "" }),
+		t({ "  <title>" }),
+		i(1),
+		t({ "</title>", "" }),
+		t({ "</head>", "" }),
+		t({ "<body>", "" }),
+		i(0),
+		t({ "", "</body>", "" }),
+		t({ "</html>" }),
+	}),
 }, {
 	key = "php",
 })
