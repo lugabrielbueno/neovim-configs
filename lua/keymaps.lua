99dc3938e8c9be2c1b301f5682c8109ef945f0d2
Lucas Gabriel Bueno <lbueno.gabriel@gmail.com>
2022-10-18 11:21:15 -0300

    updt new keys for split windows, write and quit

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -11,12 +11,31 @@ vim.api.nvim_set_keymap("n", "<leader>fb", ":Telescope buffers<cr>", { noremap =
 vim.api.nvim_set_keymap("n", "<leader>fh", ":Telescope help_tags<cr>", { noremap = true, silent = true })
 
 --formatting
-vim.api.nvim_set_keymap("n", "<space>fm", ":lua vim.lsp.buf.format({ async = true })<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap(
+	"n",
+	"<space>fm",
+	":lua vim.lsp.buf.format({ async = true })<cr>",
+	{ noremap = true, silent = true }
+)
 
 -- new file
 vim.api.nvim_set_keymap("n", "<leader>n", ":enew<cr>", { noremap = true, silent = true })
 
+-- navigate between .config files
 vim.api.nvim_set_keymap("n", "<leader>fd", ":edit " .. home .. "/.config<cr>", { noremap = true, silent = true })
+
+-- vertical and horizontal splits
+vim.api.nvim_set_keymap("n", "<leader>vs", ":vsplit<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>hs", ":split<cr>", { noremap = true, silent = true })
+
+-- quit window
+vim.api.nvim_set_keymap("n", "<leader>q", ":q<cr>", { noremap = true, silent = true })
+
+-- write document
+vim.api.nvim_set_keymap("n", "<leader>w", ":w<cr>", { noremap = true, silent = true })
+
+-- write and quit
+vim.api.nvim_set_keymap("n", "<leader>wq", ":wq<cr>", { noremap = true, silent = true })
 
 --jump or expand snippets
 vim.cmd("imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>' ")
