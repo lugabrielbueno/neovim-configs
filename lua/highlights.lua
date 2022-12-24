e4d9c9b4124eb18de4f6d86b4c13bf8ebd48b672
Gabriel Bueno <lbueno.gabriel@gmail.com>
2022-12-24 14:59:15 -0300

    updt hl

--- lua/highlights.lua
+++ lua/highlights.lua
@@ -1,18 +1,46 @@
 vim.api.nvim_set_hl(0, "Normal", { ctermbg = "NONE", bg = "NONE" })
 vim.api.nvim_set_hl(0, "EndOfBuffer", { ctermbg = "NONE", bg = "NONE" })
+
 vim.api.nvim_set_hl(0, "NvimTreeNormal", { ctermbg = "NONE", bg = "NONE" })
 vim.api.nvim_set_hl(0, "NormalFloat", { ctermbg = "NONE", bg = "NONE" })
 vim.api.nvim_set_hl(0, "NormalNC", { ctermbg = "NONE", bg = "NONE" })
 vim.api.nvim_set_hl(0, "TelescopeNormal", { ctermbg = "NONE", bg = "NONE" })
 vim.api.nvim_set_hl(0, "FloatBorder", { ctermbg = "NONE", bg = "NONE" })
-vim.api.nvim_set_hl(0, "DiagnosticFloatingWarn", { fg = "#ceb42f", bg = "NONE" })
+
+
+-- Warning
 vim.api.nvim_set_hl(0, "LspDiagnosticsSignWarning", { fg = "#ceb42f", bg = "NONE" })
 vim.api.nvim_set_hl(0, "LspDiagnosticsDefaultWarning", { fg = "#ceb42f", bg = "NONE" })
 vim.api.nvim_set_hl(0, "DiagnosticWarn", { fg = "#ceb42f", bg = "NONE" })
 vim.api.nvim_set_hl(0, "LspDiagnosticsSignWarn", { fg = "#ceb42f", bg = "NONE" })
 vim.api.nvim_set_hl(0, "WarningMsg", { fg = "#ceb42f", bg = "NONE" })
+vim.api.nvim_set_hl(0, "DiagnosticFloatingWarn", { fg = "#ceb42f", bg = "NONE" })
+
+-- Error
+vim.api.nvim_set_hl(0, "LspDiagnosticsSignError", { fg = "#e67e80", bg = "NONE" })
+vim.api.nvim_set_hl(0, "LspDiagnosticsDefaultError", { fg = "#e67e80", bg = "NONE" })
+vim.api.nvim_set_hl(0, "DiagnosticError", { fg = "#e67e80", bg = "NONE" })
+vim.api.nvim_set_hl(0, "LspDiagnosticsSignError", { fg = "#e67e80", bg = "NONE" })
+vim.api.nvim_set_hl(0, "ErrorMsg", { fg = "#e67e80", bg = "NONE" })
 vim.api.nvim_set_hl(0, "DiagnosticFloatingError", { fg = "#e67e80", bg = "NONE" })
-vim.api.nvim_set_hl(0, "DiagnosticFloatingHint", { fg = "LightGrey", bg = "NONE" })
+
+
+-- Hint
+vim.api.nvim_set_hl(0, "LspDiagnosticsSignHint", { fg = "Purple", bg = "NONE" })
+vim.api.nvim_set_hl(0, "LspDiagnosticsDefaultHint", { fg = "Purple", bg = "NONE" })
+vim.api.nvim_set_hl(0, "DiagnosticHint", { fg = "Purple", bg = "NONE" })
+vim.api.nvim_set_hl(0, "LspDiagnosticsSignHint", { fg = "Purple", bg = "NONE" })
+vim.api.nvim_set_hl(0, "HintMsg", { fg = "Purple", bg = "NONE" })
+vim.api.nvim_set_hl(0, "DiagnosticFloatingHint", { fg = "Purple", bg = "NONE" })
+
+-- Info
+vim.api.nvim_set_hl(0, "LspDiagnosticsSignInfo", { fg = "LightBlue", bg = "NONE" })
+vim.api.nvim_set_hl(0, "LspDiagnosticsDefaultInfo", { fg = "LightBlue", bg = "NONE" })
+vim.api.nvim_set_hl(0, "DiagnosticInfo", { fg = "LightBlue", bg = "NONE" })
+vim.api.nvim_set_hl(0, "LspDiagnosticsSignInfo", { fg = "LightBlue", bg = "NONE" })
+vim.api.nvim_set_hl(0, "InfoMsg", { fg = "LightBlue", bg = "NONE" })
+vim.api.nvim_set_hl(0, "DiagnosticFloatingInfo", { fg = "LightBlue", bg = "NONE" })
+
 vim.api.nvim_set_hl(0, "DiagnosticFloatingInfo", { fg = "LightBlue", bg = "NONE" })
 vim.api.nvim_set_hl(0, "CmpItemKindVariable", { bg = "NONE", fg = "LightBlue" })
 vim.api.nvim_set_hl(0, "CmpItemKindText", { bg = "NONE", fg = "LightBlue" })
@@ -28,9 +56,5 @@ vim.api.nvim_set_hl(0, "DashboardCenter", { bg = "NONE", fg = "#6c959b" })
 vim.api.nvim_set_hl(0, "DashboardFooter", { bg = "NONE", fg = "#278ead" })
 vim.api.nvim_set_hl(0, "DashboardShortcut", { bg = "NONE", fg = "#278ead" })
 vim.api.nvim_set_hl(0, "DashboardHeader", { bg = "NONE", fg = "#d6d6d6" })
-vim.api.nvim_set_hl(
-	0,
-	"HeaderDiagnosticOpenFloat",
-	{ strikethrough = true, bg = "NONE", fg = "#ceb42f", bold = true, underdouble = true }
-)
+vim.api.nvim_set_hl(0, "HeaderDiagnosticOpenFloat", { bg = "NONE", fg = "#ffffff", bold = true })
 --"#dbbc7f"
