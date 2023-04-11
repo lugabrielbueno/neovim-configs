65dc91aabfb8cb3017f53dd8e13ac63a20092800
Lucas G. Bueno <lbueno.gabriel@gmail.com>
2023-04-10 21:10:46 -0300

    updt

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -23,8 +23,8 @@ vim.api.nvim_set_keymap(
 
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
-vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
+vim.keymap.set("n", "<leader>sl", vim.diagnostic.open_float)
+vim.keymap.set("n", "<leader>sb", vim.diagnostic.setloclist)
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
 
