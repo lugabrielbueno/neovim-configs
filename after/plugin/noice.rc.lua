7fa80e204f7314a45c30a8995b03d89344a04566
Lucas G. Bueno <lbueno.gabriel@gmail.com>
2023-04-22 11:08:56 -0300

    updt

--- after/plugin/noice.rc.lua
+++ after/plugin/noice.rc.lua
@@ -1,7 +1,15 @@
 local loaded, noice = pcall(require, "noice")
+local loaded_not, notify = pcall(require, "notify")
 if not loaded then
 	return
 end
 noice.setup({
+	-- add any options here
+})
+if not loaded_not then
+	return
+end
+notify.setup({
+	background_colour = "#0000000",
 	-- add any options here
 })
